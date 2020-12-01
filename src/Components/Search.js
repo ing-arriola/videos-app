4a08d31720387bee75eaec9ebde1a119da52e665
ing-arriola <ingeniero.arriola@gmail.com>
2020-12-01 09:45:17 -0600

    first commit

---
+++ src/Components/Search.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Row, Form } from "react-bootstrap";
+import { useState } from "react";
+
+const Search = () => {
+  const [term, setTerm] = useState("");
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Container className="py-4">
+      <Form onSubmit={onFormSubmit}>
+        <Form.Label>Search</Form.Label>
+        <Form.Control
+          onChange={(e) => setTerm(e.target.value)}
+          value={term}
+          type="text"
+          placeholder="search a video"
+        />
+      </Form>
+    </Container>
+  );
+};
+
+export default Search;
